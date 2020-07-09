105276ba184e6aaaf9bdd70c22b730079957354f
smaccary <smaccary@student.42.fr>
2020-07-09 12:45:50 +0200

    saving ...

--- src/start.sh
+++ src/start.sh
@@ -1,8 +1,8 @@
 echo AUTO_INDEX : $AUTO_INDEX
-if  [ "${AUTO_INDEX}"=="OFF" ]
+if  [ "${AUTO_INDEX}" == "OFF" ]
     then
-		echo AUTO_INDEX OFF
+		echo "AUTO_INDEX OFF"
     	mv sites-enabled/default_auto_index_off /etc/nginx/sites-enabled/default
     else
-		echo AUTO_INDEX ON
+	    echo "AUTO_INDEX ON (default)"
 fi
